1e671fdfea1561be3386ed6f676a5fc323ae902e
lin <linhuang@yeahka.com>
2023-11-29 15:09:05 +0800

    add earthquake demo

---
+++ eq_data/eq_explore _data.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import json
+
+# Read data as a string and convert to a Python object.
+path = Path("eq_data_1_day_m1.geojson")
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+# Create a more readable version of the data file.
+
+# path = Path("readable_eq_data.geojson")
+# readable_contents = json.dumps(all_eq_data, indent=4)
+
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data["features"]
+print(len(all_eq_dicts))
