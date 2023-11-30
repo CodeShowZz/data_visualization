8fe884ef1f01f7a8044b819b8f26f99692a5ad7c
lin <linhuang@yeahka.com>
2023-11-30 17:14:11 +0800

    add some date visual

--- eq_data/eq_explore _data.py
+++ eq_data/eq_explore _data.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import plotly.express as px
 import json
 
 # Read data as a string and convert to a Python object.
-path = Path("eq_data_1_day_m1.geojson")
-contents = path.read_text()
+path = Path("eq_data_30_day_m1.geojson")
+contents = path.read_text(encoding="utf-8")
 all_eq_data = json.loads(contents)
 
 # Create a more readable version of the data file.
@@ -13,4 +14,31 @@ all_eq_data = json.loads(contents)
 
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data["features"]
-print(len(all_eq_dicts))
+mags, lons, lats, eq_titles = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict["properties"]["mag"]
+    lon = eq_dict["geometry"]["coordinates"][0]
+    lat = eq_dict["geometry"]["coordinates"][1]
+    eq_title = eq_dict["properties"]["title"]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    eq_titles.append(eq_title)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+
+title = "Global Earthquakes"
+fig = px.scatter_geo(
+    lat=lats,
+    lon=lons,
+    size=mags,
+    title=title,
+    color=mags,
+    color_continuous_scale="Viridis",
+    labels={"color": "Magnitude"},
+    projection="natural earth",
+    hover_name=eq_titles,
+)
+fig.show()
